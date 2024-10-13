40dae73f5d4bcfb1510525c30bf3b4d380989721
Naman Sharma <namanks0511@gmail.com>
2024-10-13 12:46:36 +0530

    test

--- android/app/src/main/kotlin/com/example/alarm_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/alarm_app/MainActivity.kt
@@ -1,5 +1,27 @@
 package com.example.alarm_app
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example/alarm-app";
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+        // This method is invoked on the main thread.
+        call, result ->
+            if (call.method == "printHelloWorld") {
+                printHelloWorld()
+            } else {
+                result.notImplemented()
+            }
+        }
+        
+    }
+    private fun printHelloWorld() {
+            println("Hello World!")
+        }
+}
